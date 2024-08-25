4d0de7387780190765323e2a9077c87cdbfe42e7
OfficialAvenger <120937975+OfficialAvenger@users.noreply.github.com>
2024-08-25 14:00:13 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -48,7 +48,7 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("<b>ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ....</b>")
+        temp_msg = await message.reply("ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ....")
         try:
             messages = await get_messages(client, ids)
         except:
